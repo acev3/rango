ae1594e27ae5f7d502a9d3e76cd4c3829f71b07a
acev3 <kovalchukilya5@gmail.com>
2020-02-07 14:12:05 +0300

    Step 9

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rango.models import Category, Page
 from django.http import HttpResponse
+from rango.forms import CategoryForm, PageForm
 
 """
 def index(request):
@@ -43,7 +44,7 @@ def category(request, category_name_slug):
         # Итак метод .get() возвращает экземпляр модели или вызывает исключение.
         category = Category.objects.get(slug=category_name_slug)
         context_dict['category_name'] = category.name
-
+        context_dict['category_name_slug'] = category.slug
         # Получит ьвсе связанные страницы.
         # Заметьте, что фильтр возвращает >= 1 экземпляр модели.
         pages = Page.objects.filter(category=category)
@@ -60,3 +61,54 @@ def category(request, category_name_slug):
 
     # Возврщаем ответ на запрос клиенту.
     return render(request, 'rango/category.html', context_dict)
+
+
+def add_category(request):
+    # HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Все поля формы были заполнены правильно?
+        if form.is_valid():
+            # Сохранить новую категорию в базе данных.
+            form.save(commit=True)
+
+            # Теперь вызвать предсталвение index().
+            # Пользователю будет показана главная страница.
+            return index(request)
+        else:
+            # Обрабатываемая форма содержит ошибки - вывести их в терминал.
+            print(form.errors)
+    else:
+        # Если запрос был не POST, вывести форму, чтобы можно было ввести в неё данные.
+        form = CategoryForm()
+
+    # Форма с ошибкой (или ошибка с данных), форма не была получена...
+    # Вывести форму с сообщениями об ошибках (если они были).
+    return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+                cat = None
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                # вероятно здесь лучше использовать redirect.
+                return category(request, category_name_slug)
+        else:
+            print(form.errors)
+    else:
+        form = PageForm()
+
+    context_dict = {'form': form, 'category': cat,  'category_name_slug': category_name_slug}
+
+    return render(request, 'rango/add_page.html', context_dict)
